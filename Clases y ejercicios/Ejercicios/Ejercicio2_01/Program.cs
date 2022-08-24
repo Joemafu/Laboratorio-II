a53ab4f4090f96b1a7fcd579b7dcace036b30344
Joel <joe_mafu@hotmail.com>
2022-08-24 12:38:54 -0300

    Clase 2

---
+++ Clases y ejercicios/Ejercicios/Ejercicio2_01/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Ejercicio2_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            bool entradaValida;
+            int entrada;
+            int max = 0;
+            int min = 0;
+            int acumulador = 0;
+            float promedio;
+            bool flag = false;
+
+            for (i=1;i<11;i++)
+            {
+
+                do
+                {
+                    Console.WriteLine("Ingrese un número: (" + i +  ")");
+                    entradaValida = int.TryParse(Console.ReadLine(), out entrada);
+                } while (!entradaValida || !Validador.Validar(entrada, -100, 100));
+
+                acumulador += entrada;
+
+                if (flag == false || entrada < min)
+                {
+                    min = entrada;
+
+                }
+                if (flag == false || entrada > max)
+                {
+                    max = entrada;
+                }
+                flag = true;
+
+
+            }
+
+            promedio = (float)acumulador / 10;
+
+            Console.WriteLine("\n\nEl máximo valor es {0}, el mínimo valor es {1} y el promedio de los ingresados es {2}", max, min, promedio);
+        }
+    }
+}
---
+++ Clases y ejercicios/Ejercicios/Ejercicio2_01/Validador.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2_01
+{
+    class Validador
+    {
+
+        /// <summary>
+        /// Validar numero dentro de un rango
+        /// </summary>
+        /// <param name="valor">dato a validar.</param>
+        /// <param name="min">mínimo valor incluido. </param>
+        /// <param name="max">máximo valor incluido.</param>
+        /// <returns></returns>
+        public static bool Validar(int valor, int min, int max)
+        {
+            bool ret = false;
+
+            if (valor < max && valor > min)
+            {
+                ret = true;
+            }
+
+            return ret;
+        }
+    }
+}
