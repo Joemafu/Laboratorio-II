a7b73285986802eaa33169b7f296942d006becc3
Joel Mahafud <joe_mafu@hotmail.com>
2022-09-09 17:03:27 -0300

    Clase 4 ejercicios

--- Clases y ejercicios/Ejercicios/Ejercicio02_03/Conversor.cs
+++ Clases y ejercicios/Ejercicios/Ejercicio02_03/Conversor.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Ejercicio02_03
 {
-    public class Conversor
+    class Conversor
     {
         public static string ConvertirDecimalABinario(int numeroEntero)
         {
